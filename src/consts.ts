afb9735ab80dd412b66f992e282aa7d5f0a20712
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2025-04-02 01:35:42 -0300

    chore: Actualizar constantes

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Apúntame Esta";
-export const SITE_DESCRIPTION = "Bienvenido a mi sitio web!";
+export const SITE_TITLE = "Los Dísipulos";
+export const SITE_DESCRIPTION =
+  "Bienvenido a los disipulos, aquí podrás encontrar resúmenes de clases, extractos de libros, resoluciones de guías y mucho más, todo presentado en formatos cortos y estructurados para facilitar su lectura y comprensión.";
 
 export const GITHUB_SITE = "https://github.com/Mr-ZeMoreno/";
